0626c5a625e2eef8c6ee1b2e9a258574ba22621c
Alex Jackson <51167460+ajax7778@users.noreply.github.com>
2019-10-03 13:57:29 -0400

    Update msp430g2553M.c

--- Multiple Blink/MSP430G2553/msp430g2553M.c
+++ Multiple Blink/MSP430G2553/msp430g2553M.c
@@ -68,14 +68,14 @@ int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
   P1DIR |= 0x01;                            // Set P1.0 to output direction
-  P1DIR |= 0x60;
+  P1DIR |= 0x60;                            // Set P1.6 to output direction
 
   for (;;)
   {
     volatile unsigned int i;
 
     P1OUT ^= 0x01;                          // Toggle P1.0 using exclusive-OR
-    P1OUT ^= 0x60;
+    P1OUT ^= 0x60;                          // Toggle P1.6 using exclusive-OR
 
     i = 50000;                              // Delay
     do (i--);
